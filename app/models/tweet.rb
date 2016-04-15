b641246e1d45c0dfd2af434866a1dc34e6bd66a0
ashante <ashantejr@gmail.com>
2016-04-15 16:15:31 +0000

    you don't gotta write shit

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,15 +1,3 @@
-class Tweet
-  attr_accessor :username, :status
-  
-  ALL_TWEETS = []
-
-  def initialize(username, status)
-    @username = username
-    @status = status
-    ALL_TWEETS << self
-  end
-
-  def self.all
-    ALL_TWEETS
-  end
+class Tweet < ActiveRecord::Base
+ 
 end
